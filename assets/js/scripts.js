9f34cbdf825141c0c6a65cb11634c0684260d92d
Josh Harrison <josh_harrison@intuit.com>
2024-08-14 11:12:51 -0600

    updates

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,13 @@
 (function() {
     'use strict';
 
-    // Your code here
-    console.log('Hello, World!');
+    const navListLinks = document.querySelectorAll('.nav-item > a');
+    const windowHref = window.location.href;
+    navListLinks.forEach(link => {
+        if (link.href === windowHref) {
+            link.classList.add('active');
+        } else {
+            link.classList.remove('active');
+        }
+    });
 })();
