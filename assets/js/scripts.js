fedd53b7e7274666a2ffd2a0012b603bcffe1645
Josh Harrison <josh_harrison@intuit.com>
2024-08-16 09:04:09 -0600

    updates

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -25,15 +25,39 @@
         const menu = document.querySelector('.nav-list-container');
         menu.classList.toggle('show');
         menu.setAttribute('aria-expanded', 'true');
+        menu.querySelectorAll('a').forEach((link) => {
+            link.setAttribute('tabindex', '-1');
+        });
+        menu.querySelector('a').focus();
+        // add a keydown event listener to the menu for up and down arrow keys
+        menu.addEventListener('keydown', (event) => {
+            if (event.key === 'ArrowDown') {
+                event.preventDefault();
+                const nextLink = event.target.nextElementSibling;
+                if (nextLink) {
+                    nextLink.focus();
+                } else {
+                    menu.querySelector('a').focus();
+                }
+            } else if (event.key === 'ArrowUp') {
+                event.preventDefault();
+                const prevLink = event.target.previousElementSibling;
+                if (prevLink) {
+                    prevLink.focus();
+                } else {
+                    menu.querySelector('a:last-child').focus();
+                }
+            }
+        });
         setTimeout(() => {
             document.addEventListener('click', closeMenu);
-
         });
     }
 
     const menuButton = document.querySelector('.menu-toggle');
     menuButton.addEventListener('click', openMenu);
     menuButton.addEventListener('keydown', (event) => {
+        debugger;
         ['Escape', 'Tab'].includes(event.key) && closeMenu(event);
     });
 })();
